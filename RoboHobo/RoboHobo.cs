45dc948d9e7c2e2013d837fff7c7676c6f0f66da
Albert Corsten <corstena@uw.edu>
2017-07-24 13:43:27 -0700

    Added whenispax, lenny, and commands

--- RoboHobo/RoboHobo.cs
+++ RoboHobo/RoboHobo.cs
@@ -24,18 +24,30 @@ namespace RoboHobo
 
             discord.UsingCommands(x =>
             {
-                x.PrefixChar = '!';
+                x.PrefixChar = '~';
                 x.AllowMentionPrefix = true;
             });
 
             commands = discord.GetService<CommandService>();
 
+            registerHelp();
             registerFbi();
+            registerLenny();
+            registerWhenIsPax();
 
             discord.ExecuteAndWait(async () =>
             {
-                await discord.Connect("MzM4OTM0MDA3ODAwNTI4ODk5.DFcvQg.lDurq8F8mcRyfEOxZk-rDCkjZis", TokenType.Bot);
+                await discord.Connect("MzM5MTMyNTgxNzgwMTI3NzQ1.DFfjBg.7ti7r5WzQZgrwUT4LhHOln7VOwM", TokenType.Bot);
             });
+        }
+
+        private void registerHelp()
+        {
+            commands.CreateCommand("commands")
+                .Do(async (e) =>
+                {
+                   await e.Channel.SendMessage("The current list of commands are: \"~fbi\", \"~lenny\", \"~whenispax\"");
+                });
         }
 
         private void registerFbi()
@@ -44,6 +56,35 @@ namespace RoboHobo
                 .Do(async (e) =>
                 {
                     await e.Channel.SendFile("images/fbi1.gif");
+                });
+        }
+
+        private void registerLenny()
+        {
+            commands.CreateCommand("lenny")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("( ͡° ͜ʖ ͡°)");
+                });
+        }
+
+        private void registerWhenIsPax()
+        {
+            commands.CreateCommand("whenispax")
+                .Do(async (e) =>
+                {
+                    DateTime paxStart = new DateTime(2017, 9, 2, 8 ,0 , 0);
+                    DateTime currentTime = DateTime.Now;
+                    var message = "";
+                    if(paxStart.CompareTo(currentTime) > 0)
+                    {
+                        message = currentTime.Day + " days, " + currentTime.Hour + " hours, " + currentTime.Minute + " minutes, and " + currentTime.Second + " seconds until PAX West 2017!";
+                    } else
+                    {
+                        message = "PAX West 2017 is over!";
+                    }
+
+                    await e.Channel.SendMessage(message);
                 });
         }
 
