be7b81eef175fc636116b14f029df6bcedcf8dfc
Albert Corsten <corstena@uw.edu>
2017-07-24 00:28:30 -0700

    Add project files.

---
+++ RoboHobo/RoboHobo.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboHobo
+{
+    class RoboHobo
+    {
+        DiscordClient discord;
+
+        public RoboHobo()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '/';
+                x.AllowMentionPrefix = true;
+            });
+
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("Hello")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Hi!");
+                });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MzM4OTM0MDA3ODAwNTI4ODk5.DFcvQg.lDurq8F8mcRyfEOxZk-rDCkjZis", TokenType.Bot);
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
